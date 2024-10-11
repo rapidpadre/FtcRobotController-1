ebecd2626808488c5882127447f04c334b28a4f5
Team RoboGenesis <115941832+TeamRoboGenesis@users.noreply.github.com>
2024-10-11 13:19:05 -0700

    Update teleop.java
    needs fixing. Last changed, 10/9/24

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -9,9 +9,10 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "teleop")
 public class teleop extends OpMode {
     public DcMotor frontLeftWheel   = null;
-    public DcMotor  frontRightWheel  = null;
+    public DcMotor frontRightWheel  = null;
     public DcMotor backLeftWheel = null;
     public DcMotor backRightWheel = null;
+    public Servo mainIntake = null;
 
     @Override
     public void init() {
@@ -19,14 +20,25 @@ public class teleop extends OpMode {
         frontRightWheel = hardwareMap.get(DcMotor.class, "frontRight");
         backLeftWheel = hardwareMap.get(DcMotor.class, "backLeft");
         backRightWheel = hardwareMap.get(DcMotor.class, "backRight");
-
+        mainIntake = hardwareMap.get(Servo.class, "mainIntake");
 
     }
 
     @Override
     public void loop() {
-        if (gamepad1.a) {
-        frontRightWheel.setPower(0.5);
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        frontLeftWheel.setPower(y + x + rx);
+        backLeftWheel.setPower(y - x + rx);
+        frontRightWheel.setPower(y - x - rx);
+        backRightWheel.setPower(y + x - rx);
+        if (gamepad1.b) {
+            mainIntake.setPosition(0.1);
+        } else if (gamepad1.x) {
+            mainIntake.setPosition(0.2);
         }
+
     }
 }
